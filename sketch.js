840106285182693e6be7771f4f7a5db23ffe2c96
Cameron Moore <cammoo37@gmail.com>
2016-10-10 17:44:08 -0500

    Second attempt - with images

--- sketch.js
+++ sketch.js
@@ -1,6 +1,12 @@
 function preload()
 {
   music = loadSound('sound/the_end_of_time.mp3');
+
+  webSurfing = loadImage("img/web_surfing");
+  nyanCat = loadImage("img/nyan_cat");
+
+  canvasWidth = webSurfing.width;
+  canvasHeight = weSurfing.height;
 }
 
 function setup()
@@ -8,6 +14,8 @@ function setup()
   createCanvas(800,400);
   music.setVolume(0.1);
   music.play();
+
+  image(webSurfing, 0,0, webSurfing.width, webSurfing.height);
 }
 
 
